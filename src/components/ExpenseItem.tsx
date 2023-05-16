e6c845af76b4f67f0571b146847889d78c856953
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2023-05-17 04:37:48 +0800

    add expense props

---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+function ExpenseItem(props: any) {
+    return (
+        <div className="expense-item">
+            <div>{props.date.toLocaleString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
